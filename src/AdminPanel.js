c0a7ed7add9f1116d668b5d7daf5f8213addbce3
Victor Segui <victorapostoles@gmail.com>
2025-07-29 19:23:47 -0300

    Proyecto base Dispen-Easy Web

---
+++ src/AdminPanel.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AdminPanel() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Panel de Administración - Dispen-Easy</h1>
+      <p>Aquí se podrán ver los pagos, controlar dispensadores, etc.</p>
+    </div>
+  );
+}
+
+export default AdminPanel;
